a137ca63f780816e66ca2b1ed5cffc47ad8b3c21
SulaimanMydeen <sulaimanksmcse@gmail.com>
2019-03-09 22:11:07 +0530

    commit check

--- news-scraper/src/main/java/com/newsscraper/NewsScraperApplication.java
+++ news-scraper/src/main/java/com/newsscraper/NewsScraperApplication.java
@@ -3,6 +3,10 @@ package com.newsscraper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Sulaiman Mydeen K
+ *
+ */
 @SpringBootApplication
 public class NewsScraperApplication {
 
